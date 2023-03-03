908246b53e0a9319da14dc9b562b20d4fc90dd4b
C0iAS <sebapl09@gmail.com>
2023-03-02 20:25:22 -0500

    Models

---
+++ main.py
@@ -0,0 +1,89 @@
+# Python
+from uuid import UUID
+from datetime import date
+from datetime import datetime
+from typing import Optional, List
+
+# Pydantic
+from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
+
+# FastAPI
+from fastapi import FastAPI
+from fastapi import status
+
+app = FastAPI()
+
+# Models
+
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ..., 
+        min_length=8,
+        max_length=64
+    )
+
+class User(UserBase):
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
+
+class Quick(BaseModel):
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ..., 
+        min_length=1, 
+        max_length=256
+    )
+    created_at: datetime = Field(default=datetime.now())
+    update_at: Optional[datetime] = Field(default=None)
+    by: User = Field(...)
+
+## Path operations
+
+@app.get(path="/")
+def home():
+    return {"Quicker API": "Working!"}
+
+@app.post(
+    path="/singup",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags={"Users"}
+)
+def signup():
+    pass
+
+@app.post(
+    path="/login",
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary="Login a User",
+    tags={"Users"}
+)
+def login():
+    pass
+
+@app.post(
+    path="/users",
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary="Register a User",
+    tags={"Users"}
+)
+def signup():
+    pass
